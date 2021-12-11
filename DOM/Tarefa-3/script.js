1540a25dc70057c63d61ff09833883491e159c50
MateusFrei <41398101+MateusFrei@users.noreply.github.com>
2021-12-10 22:15:44 -0300

    upado

---
+++ DOM/Tarefa-3/script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var x = document.getElementById("linha");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction2(){
+  var x2 = document.getElementById("linha2");
+  if (x2.style.display === "none") {
+    x2.style.display = "block";
+  } else {
+    x2.style.display = "none";
+  }
+}
+function myFunction3(){
+  var x3 = document.getElementById("linha3");
+  if (x3.style.display === "none") {
+    x3.style.display = "block";
+  } else {
+    x3.style.display = "none";
+  }
+}
